75fec5310775c7b20aaf0971830efc54a262020c
Zumrad Kabilova <zkabilova@gmail.com>
2021-10-28 15:38:45 -0400

    Update Distance.java
    Adding some Java Docs

--- Distance.java
+++ Distance.java
@@ -1,20 +1,41 @@
+/**
+ * Represents a distance between two locations.
+ */
 public class Distance {
-    // member variables of Distance object
+
+    //instance variables
+    /**
+     * First location.
+     */
     private Location loc1;
+    /**
+     * Second location.
+     */
     private Location loc2;
+
     // distance constructor
+    /**
+     * Creates a Distance object with two given locations.
+     * @param location1 first location
+     * @param location2 second location
+     */
     public Distance(Location location1, Location location2){
         this.loc1 = location1;
         this.loc2 = location2;
     }
     // calculating the distance between loc1 and loc2
+
+    /**
+     * Calculates the distance between the two locations.
+     * @return distance between the two locations
+     */
     public double CalcDist(){
         // radius of the Earth in meters
         double rad = 6378*1000;
-        // extracting the latitude and longitude of the loc 1
+        // extracting the latitude and the longitude of the first location
         double lat1 = loc1.getLatitude();
         double lon1 = loc1.getLongitude();
-        //extracting the latitude and longitude of the loc 2
+        //extracting the latitude and the longitude of the second location
         double lat2 = loc2.getLatitude();
         double lon2 = loc2.getLongitude();
         // calculating the distance
